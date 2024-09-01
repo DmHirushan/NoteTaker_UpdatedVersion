6005d1798763a26ce4ad71db048329f76b9dcca7
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-09-01 14:11:37 +0530

    user entity properties added!

--- src/main/java/lk/ijse/gdse/aad68/notetaker/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/entity/UserEntity.java
@@ -1,6 +1,32 @@
 package lk.ijse.gdse.aad68.notetaker.entity;
 
-import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.Serializable;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "users")
+@Entity
 public class UserEntity implements SuperEntity {
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Column(columnDefinition = "LONGTEXT")
+    private String profilePic;
+    private List<NoteEntity> notes;
 }
