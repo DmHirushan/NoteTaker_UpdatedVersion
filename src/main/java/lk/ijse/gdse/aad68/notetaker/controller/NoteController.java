d43887e8c479b3db2483baa4d627f61fbf4346fd
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-09-29 12:22:34 +0530

    updateNote validated!

--- src/main/java/lk/ijse/gdse/aad68/notetaker/controller/NoteController.java
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/controller/NoteController.java
@@ -60,6 +60,10 @@ public class NoteController {
     @PatchMapping(value = "/{noteId}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> updateNote(@PathVariable ("noteId") String noteId, @RequestBody NoteDTO note) {
         try{
+            if (note == null && (noteId == null || note.equals(""))) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+
             noteService.updateNote(noteId, note);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (NoteNotFoundException e){
